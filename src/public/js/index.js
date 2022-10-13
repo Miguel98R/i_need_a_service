92e7e5e8d737e7cf11c6a66e131206b0a5eda2ca
mikeleganux1998 <111320389+mikeleganux1998@users.noreply.github.com>
2022-10-13 09:42:03 -0500

    funcion de rutas

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -2,27 +2,28 @@ const notyf = new Notyf()
 
 $(document).ready(function () {
 
-    let nuevo_usuario = {}
-    let user
+  let nuevo_usuario = {}
+  let user
 
 
-    $('#guardar_user').click(async function () {
+  $('#guardar_user').click(async function () {
 
-        
 
-            nuevo_usuario.nombre_user = $('#nombre_user').val()
-            nuevo_usuario.edad = $('#edad_user').val()
-            nuevo_usuario.email = $('#email_user').val()
-            nuevo_usuario.password = $('#password_user').val()
 
-        console.log(nuevo_usuario)
-      
-       
-        api_conection('POST','/api/user/newUser',nuevo_usuario,function(){
-          console.log("Si se pudo")
-        })
+    nuevo_usuario.nombre_user = $('#nombre_user').val()
+    nuevo_usuario.edad = $('#edad_user').val()
+    nuevo_usuario.email = $('#email_user').val()
+    nuevo_usuario.password = $('#password_user').val()
 
+    console.log(nuevo_usuario)
+
+
+    api_conection('POST', '/api/user/newUser', nuevo_usuario, function () {
+      console.log("Si se pudo")
+      notyf.success("Usuario creado")
     })
+
+  })
 
 })
 
