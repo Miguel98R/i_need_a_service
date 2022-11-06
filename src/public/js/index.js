23205f266225cafeae84d5367bfc8599a019f058
mikeleganux1998 <111320389+mikeleganux1998@users.noreply.github.com>
2022-11-06 10:50:08 -0600

    menu servicios

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -2,40 +2,40 @@ let services_menu = [
   {
   
     title: 'Entretenimiento',
-    img:''
+    img:'https://www.deyaalvarezvillajuana.com/wp-content/uploads/2019/02/pol%C3%ADtica-del-entretenimiento-945x610.jpg'
 
 }, 
 {
 
   
     title: 'Belleza',
-    img:''
+    img:'https://www.xeouradio.com/wp-content/uploads/2022/09/belleza.jpg'
     
 }, 
 {
 
  
     title: 'Hogar',
-    img:''
+    img:'https://prodesa.com/wp-content/uploads/2021/05/iStock-1209431211-1.jpg'
    
 }, 
 {
   
   title: 'Empresarial',
-  img:''
+  img:'https://www.marista.edu.mx/files/media/image/program_04079f13161f39bf9eb88d620a1b12a7.jpg'
 },
 {
 
 
   title: 'Autos',
-  img:''
+  img:'https://www.eluniversal.com.mx/sites/default/files/2020/03/24/autos_de_lujo.jpg'
   
 }, 
 {
 
 
   title: 'Salud',
-  img:''
+  img:'https://unamglobal.unam.mx/wp-content/uploads/2021/03/facSistemas-de-salud-digital.jpg'
  
 }, 
 
@@ -43,7 +43,7 @@ let services_menu = [
 
 
   title: 'Mascotas',
-  img:''
+  img:'https://s1.eestatic.com/2020/08/26/curiosidades/mascotas/mascotas-perros-gatos_515959375_158488465_1706x960.jpg'
  
 }, 
 ]
@@ -54,16 +54,14 @@ $(document).ready(function () {
   for (let item of services_menu){
 
     $('.services_menu').append('<div class="col-4">'
-      +'<div class="card my-2 mx-3">'
+      +'<div class="  my-2 mx-3">'
 
-          +'<div class="card-header" >'
-            
+         
+          +'<div class=" bg-transparent container-fluid">'
+                +'<img class="card-img" src="'+item.img+'"></img>'
           +'</div>'
-          +'<div class="card-body">'
-                +'<img width="67%" class="img-fluid" src=""></img>'
-          +'</div>'
-          +'<div class="card-footer text-center" style="background-color: #2e679a;">'
-            + '<h5 class="text-white">'+item.title+'</h5>'
+          +'<div  class=" my-2 text-center  sombreado " >'
+            + '<button style="border: solid; border-color:#2e889a;"  class="btn text-dark">'+item.title+'</button>'
           +'</div>'
   
   
